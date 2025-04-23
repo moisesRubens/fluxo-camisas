d290f30425df435cc66f3fa1aae7edef4bf11fd5
moisesRubens <moisesrubens.pro@gmail.com>
2025-04-23 15:02:17 -0300

    a

---
+++ src/main/java/com/rosadesaron/fluxo_camisa/FluxoCamisaApplication.java
@@ -0,0 +1,13 @@
+package com.rosadesaron.fluxo_camisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoCamisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxoCamisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosadesaron/fluxo_camisa/FluxoCamisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosadesaron.fluxo_camisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxoCamisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
